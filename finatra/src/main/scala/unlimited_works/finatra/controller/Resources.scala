39a9d93798be2c7637528a787d3f3c08a00c30a6
LoranceChen <wangchen@qiaobutang.com>
2016-01-24 21:53:57 +0800

    some amazing tip

--- finatra/src/main/scala/unlimited_works/finatra/controller/Resources.scala
+++ finatra/src/main/scala/unlimited_works/finatra/controller/Resources.scala
@@ -6,11 +6,13 @@ import com.twitter.finatra.http.Controller
 /**
   * Getting all resources from the route.
   * path of assets defined at AssetsPathConfigModule.scala
+  * [Some AMAZING] It's doesn't work smoothly, sometimes, it response with a 404.
+  * such as my ico.
   */
 class Resources extends Controller {
   get("/assets/:*") { request: Request =>
     val x = response.ok.file(request.params("*"))
-//    println(s"asset - ${request.params("*")} - $x")
+    println(s"asset - ${request.params("*")} - $x")
     x
   }
 }
